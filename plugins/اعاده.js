cc15f073fe3ac075f0295ae175c9a7430bc7adb1
FangI0 <mohamedking170170170@gmail.com>
2024-12-27 21:57:17 +0200

    Update اعاده.js

--- plugins/اعاده.js
+++ plugins/اعاده.js
@@ -43,7 +43,7 @@ let handler = async (m, { conn, text }) => {
 }
 handler.help = ['reset <54xxx>']
 handler.tags = ['owner']
-handler.command = ['إعادة'] 
+handler.command = ['اعادة'] 
 handler.admin = false
 handler.rowner = true
 
